9935c4f29c6f4594825a9a938d4bb521dcf4361f
blinkbat <dbennettmiami@gmail.com>
2019-07-03 16:44:20 -0400

    keep rows for matrix-style accessing

--- js/index.js
+++ js/index.js
@@ -1,26 +1,24 @@
 
 
- // initialize board vars
- const board = [];
- let row = [];
- let playerLoc = [];
+// initialize board vars
+const board = [];
+let row = [];
+let playerLoc = [];
 
- // grab from DOM
- const root = $( "#root" );
- const log = $( "#log" );
+// grab from DOM
+const root = $( "#root" );
+const log = $( "#log" );
 
 
- // do something to test it
- 
- root.append( "I came from the JS file" );
 
- initBoard = () => {
+initBoard = () => {
     // create 10 rows
     for( i = 0; i < 10; i++ ) {
 
         // create ten cell arrs in each row
         for( j = 0; j < 10; j++ ) {
 
+            // create cell with x, y, content
             row.push({
                 x: j,
                 y: i,
@@ -29,8 +27,7 @@
 
         }
 
-        // assign current row then clear it
-        board[i] = row;
+        board.push( row );
         row = [];
 
     }
@@ -38,7 +35,46 @@
     console.log( board );
 };
 
+
+
+renderBoard = () => {
+
+    root.empty();
+
+    // for each cell
+    board.forEach( row => {
+
+        row.forEach( cell => {
+
+            let cellClass = "cell";
+
+            if( cell.content === "#" ) { cellClass = "grass"; }
+
+            if( cell.content === "#" ) { cellClass = "wall"; }
+            if( cell.content === "@" ) { cellClass = "player"; }
+            if( cell.content === "$" ) { cellClass = "foe"; }
+
+            // if not end of row,
+            if( cell.x != 9 ) {
+                // append a cell
+                root.append( `<div class="${ cellClass }">${ cell.content }</div>` );
+            } else {
+                // append a cell and linebreak
+                root.append( `<div class="${ cellClass }">${ cell.content }</div> <br/>` );
+            }
+
+        });
+
+
+    });
+
+};
+
+
+
 initBoard();
+
+renderBoard();
 
 
 
