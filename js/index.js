370b5791ead544e002a5c015de8808a1740eb2ee
blinkbat <dbennettmiami@gmail.com>
2019-07-03 15:27:43 -0400

    settin it up

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+ // initialize board vars
+ const board = [];
+ let row = [];
+ let playerLoc = [];
+
+ // grab from DOM
+ const root = $( "#root" );
+ const log = $( "#log" );
+
+
+ // do something to test it
+ 
+ root.append( "I came from the JS file" );
