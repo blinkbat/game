6ed4c8bce65c13df9958261f0cec5474f7935a41
blinkbat <dbennettmiami@gmail.com>
2019-07-03 15:51:50 -0400

    mic check

--- js/index.js
+++ js/index.js
@@ -13,3 +13,40 @@
  // do something to test it
  
  root.append( "I came from the JS file" );
+
+ initBoard = () => {
+    // create 10 rows
+    for( i = 0; i < 10; i++ ) {
+
+        // create ten cell arrs in each row
+        for( j = 0; j < 10; j++ ) {
+
+            row.push({
+                x: j,
+                y: i,
+                content: "."
+            });
+
+        }
+
+        // assign current row then clear it
+        board[i] = row;
+        row = [];
+
+    }
+
+    console.log( board );
+};
+
+initBoard();
+
+
+
+
+
+
+
+
+
+
+
